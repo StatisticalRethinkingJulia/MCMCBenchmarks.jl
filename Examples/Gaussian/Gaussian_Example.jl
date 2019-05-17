87d78720732e337e6b7acf4e9c4e0c22452e4dfe
Rob J Goedman <goedman@mac.com>
2019-05-17 15:39:48 +0200

    Minor update to Gaussian_Example.jl

--- Examples/Gaussian/Gaussian_Example.jl
+++ Examples/Gaussian/Gaussian_Example.jl
@@ -14,8 +14,11 @@ cd(ProjDir)
 #create a sampler object or a tuple of sampler objects
 #Note that AHMC and DynamicNUTS do not work together due to an error in MCMCChains:
 #https://github.com/TuringLang/MCMCChains.jl/issues/101
-samplers=(CmdStanNUTS(CmdStanConfig,ProjDir),AHMCNUTS(AHMCGaussian,AHMCconfig),
-    DHMCNUTS(sampleDHMC,2000))#,DNNUTS(DNGaussian,DNconfig))
+samplers=(
+  CmdStanNUTS(CmdStanConfig,ProjDir),
+  AHMCNUTS(AHMCGaussian,AHMCconfig),
+  DHMCNUTS(sampleDHMC,2000))
+  #DNNUTS(DNGaussian,DNconfig))
 
 #Number of data points
 Nd = [10, 100, 1000]
