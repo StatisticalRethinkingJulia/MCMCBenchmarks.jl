343ff64391d860581b02c68ef08e8e6066df1676
autodocs <autodocs>
2019-07-11 13:23:47 +0000

    build based on f17a4d0

---
+++ dev/search_index.js
@@ -0,0 +1,43 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "MCMCBenchmarks.jl Documentation",
+    "title": "MCMCBenchmarks.jl Documentation",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#MCMCBenchmarks.jl-Documentation-1",
+    "page": "MCMCBenchmarks.jl Documentation",
+    "title": "MCMCBenchmarks.jl Documentation",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "#MCMCBenchmarks.benchmark",
+    "page": "MCMCBenchmarks.jl Documentation",
+    "title": "MCMCBenchmarks.benchmark",
+    "category": "function",
+    "text": "Runs the benchmarking procedure and returns the results\n\nsamplers: a tuple of samplers or a single sampler object\nsimulate: model simulation function with keyword Nd\nNreps: number of times the benchmark is repeated for each factor combination\n\n\n\n\n\n"
+},
+
+{
+    "location": "#Functions-1",
+    "page": "MCMCBenchmarks.jl Documentation",
+    "title": "Functions",
+    "category": "section",
+    "text": "MCMCBenchmarks.benchmark"
+},
+
+{
+    "location": "#Index-1",
+    "page": "MCMCBenchmarks.jl Documentation",
+    "title": "Index",
+    "category": "section",
+    "text": ""
+},
+
+]}
