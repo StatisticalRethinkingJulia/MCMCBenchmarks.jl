7848e3c64f297c7c8e69d9c963dfff5d7971cb67
Chris Fisher <fisherc2@miamioh.edu>
2019-07-21 09:29:17 -0400

    update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,13 @@
-using MCMCBenchmarks,Test
+using MCMCBenchmarks,Test,Random
+Random.seed!(454)
 tests = [
-"generative_model_tests",
-"Gaussian_Tests",
-"SDT_Tests",
-"LBA_Tests",
-"Poisson_Test",
-"Regression_Tests"
+    "generative_model_tests",
+    "Gaussian_Tests",
+    "SDT_Tests",
+    "Poisson_Test",
+    "Regression_Tests",
+    "Plot_Tests",
+    "LBA_Tests"
 ]
 
 res = map(tests) do t
