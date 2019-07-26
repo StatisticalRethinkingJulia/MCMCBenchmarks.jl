6dcc900a437e40b62d454522cd7d1bc20e0a484b
Chris Fisher <fisherc2@miamioh.edu>
2019-07-26 15:42:17 -0400

    update display branch

--- docs/make.jl
+++ docs/make.jl
@@ -3,11 +3,12 @@ using Documenter, MCMCBenchmarks
 makedocs(
     modules = [MCMCBenchmarks],
     checkdocs = :exports,
-    authors = "Chris Fisher, Rob J Goedman",
+    authors = "Christopher R. Fisher, Rob J Goedman",
     sitename = "StatisticalRethinkingJulia/MCMCBenchmarks.jl",
     pages = Any["index.md"]
 )
 
 deploydocs(
-    repo = "github.com/StatisticalRethinkingJulia/MCMCBenchmarks.jl.git",
+    #repo = "github.com/StatisticalRethinkingJulia/MCMCBenchmarks.jl.git",
+    https://github.com/StatisticalRethinkingJulia/MCMCBenchmarks.jl/tree/docs_branch,
 )
