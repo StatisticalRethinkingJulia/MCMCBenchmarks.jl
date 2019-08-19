3590ca6f86d3d672edcf056acfd76a2356291fa4
Kai Xu <xukai921110@gmail.com>
2019-08-20 00:02:15 +0100

    fix typo

--- Examples/SDT/check_csv.jl
+++ Examples/SDT/check_csv.jl
@@ -3,4 +3,4 @@ using CSV, DataFrames, Statistics
 results_dir = ARGS[1]
 results = CSV.read("$results_dir/results.csv")
 
-by(results, [:sampler, :Nd], :d=>mean, :c=>mean, :tree_depth=>mean, :epsilon=>mean) |> println
+by(results, [:sampler, :Nd], :d_ess=>mean, :c_ess=>mean, :tree_depth=>mean, :epsilon=>mean) |> println
