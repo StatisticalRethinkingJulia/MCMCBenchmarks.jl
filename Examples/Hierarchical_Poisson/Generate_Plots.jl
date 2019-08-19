f68f528098010bd68fc155a0af70709b48ace4a4
Kai Xu <xukai921110@gmail.com>
2019-08-19 12:54:47 +0100

    clean up scripts for HP

--- Examples/Hierarchical_Poisson/Generate_Plots.jl
+++ Examples/Hierarchical_Poisson/Generate_Plots.jl
@@ -1,46 +1,12 @@
-using MCMCBenchmarks,CSV
+using MCMCBenchmarks, CSV
 
 ProjDir = @__DIR__
 cd(ProjDir)
 
-folder = "results/2019_08_03T06_11_00/"
-results = CSV.read(folder*"results.csv",copycols=true)
+# Load results
+folder = "results/2019_08_18T18_43_00/"
+results = CSV.read(folder * "results.csv", copycols=true)
 allowmissing!(results)
 
-dir = "results/"
-
-#Plot parameter recovery
-parms = Dict(:a0=>1,:a1=>.5,:a0_sig=>.5)
-recoveryPlots = plotrecovery(results,parms,(:sampler,:Nd);save=true,dir=dir)
-
-#Plot mean run time as a function of number of data points (Nd) for each sampler
-meantimePlot = plotsummary(results,:Nd,:time,(:sampler,);save=true,dir=dir,yscale=:log10)
-
-#Plot mean allocations as a function of number of data points (Nd) for each sampler
-meanallocPlot = plotsummary(results,:Nd,:allocations,(:sampler,);save=true,dir=dir,yscale=:log10,
-  ylabel="Allocations (log scale)")
-
-#Plot mean ess per second of number of data points (Nd) for each sampler
-essPS = plotsummary(results,:Nd,:ess,(:sampler,);save=true,dir=dir)
-
-#Plot density of effective sample size as function of number of data points (Nd) for each sampler
-essPlots = plotdensity(results,:ess,(:sampler,:Nd);save=true,dir=dir)
-
-#Plot density of rhat as function of number of data points (Nd) for each sampler
-rhatPlots = plotdensity(results,:r_hat,(:sampler,:Nd);save=true,dir=dir)
-
-#Plot density of time as function of number of data points (Nd) for each sampler
-timePlots = plotdensity(results,:time,(:sampler,:Nd);save=true,dir=dir)
-
-#Plot density of gc time percent as function of number of data points (Nd) for each sampler
-gcPlots = plotdensity(results,:gcpercent,(:sampler,:Nd);save=true,dir=dir)
-
-#Plot density of memory allocations as function of number of data points (Nd) for each sampler
-memPlots = plotdensity(results,:allocations,(:sampler,:Nd);save=true,dir=dir,xscale=:log10,
-  xlabel="Allocations (log scale)")
-
-#Plot density of megabytes allocated as function of number of data points (Nd) for each sampler
-megPlots = plotdensity(results,:megabytes,(:sampler,:Nd);save=true,dir=dir)
-
-#Scatter plot of epsilon and effective sample size as function of number of data points (Nd) for each sampler
-scatterPlots = plotscatter(results,:epsilon,:ess,(:sampler,:Nd);save=true,dir=dir)
+# Make plots
+include("Plotting.jl")
