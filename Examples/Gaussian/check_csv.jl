7c20d02843e6e7726bcef356772a1b739927e6b7
Kai Xu <xukai921110@gmail.com>
2019-08-19 16:06:13 +0100

    add check_csv for Gaussian

--- Examples/Gaussian/check_csv.jl
+++ Examples/Gaussian/check_csv.jl
@@ -3,4 +3,4 @@ using CSV, DataFrames, Statistics
 results_dir = ARGS[1]
 results = CSV.read("$results_dir/results.csv")
 
-by(results, [:sampler, :Nd], :mu=>mean, :sigma=>mean, :tree_depth=>mean, :epsilon=>mean) |> println
+by(results, [:sampler, :Nd], :mu_ess=>mean, :sigma_ess=>mean, :tree_depth=>mean, :epsilon=>mean) |> println
