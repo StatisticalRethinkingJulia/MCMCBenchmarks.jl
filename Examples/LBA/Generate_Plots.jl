4f82eea3d834c6fdeb871ff385add64732895ef9
Kai Xu <xukai921110@gmail.com>
2019-08-19 18:32:17 +0100

    update script for LBA

--- Examples/LBA/Generate_Plots.jl
+++ Examples/LBA/Generate_Plots.jl
@@ -5,41 +5,5 @@ cd(ProjDir)
 folder = "2019_05_30T19_40_00"
 results = CSV.read(string(ProjDir,"/results/",folder*"/results.csv"))
 
-dir = "results/"
-
-
-#Plot parameter recovery
-trueparms = Dict(Symbol("v[1]")=>1,Symbol("v[2]")=>1.5,Symbol("v[3]")=>2.5,:A=>.8,:k=>.2,:tau=>.4)
-recoveryPlots = plotrecovery(results,trueparms,(:sampler,:Nd);save=true,dir=dir)
-
-#Plot mean run time as a function of number of data points (Nd) for each sampler
-meantimePlot = plotsummary(results,:Nd,:time,(:sampler,);save=true,dir=dir,yscale=:log10)
-
-#Plot mean allocations as a function of number of data points (Nd) for each sampler
-meanallocPlot = plotsummary(results,:Nd,:allocations,(:sampler,);save=true,dir=dir,yscale=:log10,
-  ylabel="Allocations (log scale)")
-
-#Plot mean ess per second of number of data points (Nd) for each sampler
-meanallocPlot = plotsummary(results,:Nd,:ess_ps,(:sampler,);save=true,dir=dir)
-
-#Plot density of effective sample size as function of number of data points (Nd) for each sampler
-essPlots = plotdensity(results,:ess,(:sampler,:Nd);save=true,dir=dir)
-
-#Plot density of rhat as function of number of data points (Nd) for each sampler
-rhatPlots = plotdensity(results,:r_hat,(:sampler,:Nd);save=true,dir=dir)
-
-#Plot density of time as function of number of data points (Nd) for each sampler
-timePlots = plotdensity(results,:time,(:sampler,:Nd);save=true,dir=dir)
-
-#Plot density of gc time percent as function of number of data points (Nd) for each sampler
-gcPlots = plotdensity(results,:gcpercent,(:sampler,:Nd);save=true,dir=dir)
-
-#Plot density of memory allocations as function of number of data points (Nd) for each sampler
-memPlots = plotdensity(results,:allocations,(:sampler,:Nd);save=true,dir=dir,xscale=:log10,
-  xlabel="Allocations (log scale)")
-
-#Plot density of megabytes allocated as function of number of data points (Nd) for each sampler
-megPlots = plotdensity(results,:megabytes,(:sampler,:Nd);save=true,dir=dir)
-
-#Scatter plot of epsilon and effective sample size as function of number of data points (Nd) for each sampler
-scatterPlots = plotscatter(results,:epsilon,:ess,(:sampler,:Nd);save=true,dir=dir)
+# Make plots
+include("plotting.jl")
