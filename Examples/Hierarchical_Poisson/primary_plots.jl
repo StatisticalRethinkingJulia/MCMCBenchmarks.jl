63c98737c22756f151ae8d1cc807cc7714d935ef
Chris Fisher <fisherc2@miamioh.edu>
2019-08-23 06:00:55 -0400

    minor fix to plots

--- Examples/Hierarchical_Poisson/primary_plots.jl
+++ Examples/Hierarchical_Poisson/primary_plots.jl
@@ -6,7 +6,7 @@ dir = "results/"
 meantimePlot = plotsummary(results, :Nd, :time, (:sampler,); save=true, dir=dir, yscale=:log10)
 
 # Plot mean allocations as a function of number of data points (Nd) for each sampler
-meanallocPlot = plotsummary(results, :Nd, :allocations, (:sampler,); save=false, dir=dir, yscale=:log10,
+meanallocPlot = plotsummary(results, :Nd, :allocations, (:sampler,); save=true, dir=dir, yscale=:log10,
   ylabel="Allocations (log scale)"
 )
 
