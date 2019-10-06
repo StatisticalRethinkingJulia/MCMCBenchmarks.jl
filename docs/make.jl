2968fd47bebb1bb0a10a9aa920537f08d4594a51
Chris Fisher <fisherc2@miamioh.edu>
2019-10-06 07:32:19 -0400

    fix make format

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,7 @@ using Documenter, MCMCBenchmarks
 makedocs(
     modules = [MCMCBenchmarks],
     checkdocs = :exports,
-    format = Documenter.HTML(assets = ["assets/custom.css"]),
+    format = :html,
     clean = true,
     authors = "Christopher R. Fisher, Rob J Goedman",
     sitename = "StatisticalRethinkingJulia/MCMCBenchmarks.jl",
